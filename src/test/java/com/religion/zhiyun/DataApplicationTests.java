cc8eb5e393ef6b8f1dc17dbcded1a6f99e7c9b50
wangchunjiao <1120010061@qq.com>
2023-01-03 10:38:02 +0800

    xiugai

--- src/test/java/com/religion/zhiyun/DataApplicationTests.java
+++ src/test/java/com/religion/zhiyun/DataApplicationTests.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @SpringBootTest
-public class Weixin01ApplicationTests {
+public class DataApplicationTests {
     @Autowired
     DataSource dataSource;
     @Test
